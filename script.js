dc1ffa138705a587437d28c31bcb1629aac49ae8
Aakanksha258 <aakankshanarayankar8@gmail.com>
2024-12-15 16:00:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('hamburger').addEventListener('click', function () {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.style.display === 'block') {
+        sidebar.style.display = 'none';
+    } else {
+        sidebar.style.display = 'block';
+    }
+});
